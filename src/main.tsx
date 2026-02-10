1aa3f4ba0e4a5351206fd13d662bce397b31542d
wuocdat <quocdatpadpqn@gmail.com>
2026-02-10 14:45:50 +0700

    feat: Add @peculiar/webcrypto dependency and initialize Crypto in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client';
+import { Crypto } from '@peculiar/webcrypto';
 import App from './App';
-import "./index.css"
+import './index.css';
+
+if (!globalThis.crypto || !globalThis.crypto.subtle) {
+  (globalThis as any).crypto = new Crypto();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
